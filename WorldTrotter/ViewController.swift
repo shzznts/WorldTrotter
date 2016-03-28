090b3ad70931f5e3c60a5be4de8b5b84b089a8ae
Lincoln Sea <lisea@groupon.com>
2016-03-28 07:36:26 -0700

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Lincoln Sea on 3/28/16.
+//  Copyright (c) 2016 Lincoln Sea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
