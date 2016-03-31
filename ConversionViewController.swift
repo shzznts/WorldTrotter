eee8bcfaedd6dde54558ee8de9db6f37ed52f0e9
Lincoln Sea <lisea@groupon.com>
2016-03-30 18:04:18 -0700

    About to start UITabBarController

---
+++ ConversionViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Lincoln Sea on 3/29/16.
+//  Copyright (c) 2016 Lincoln Sea. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController : UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
+        let replacementTextHasDecimalSeparator = string.rangeOfString(".")
+        if existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil {
+            return false
+        }
+        
+        let charSet = NSCharacterSet.decimalDigitCharacterSet()
+        let rangeFromSet = string.rangeOfCharacterFromSet(charSet)
+        return string == "" || rangeFromSet != nil || string == "."
+    }
+
+}
