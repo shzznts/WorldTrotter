98c67e562ea83b53e7bd38ccd7f08153a4e4c0d6
Lincoln Sea <lisea@groupon.com>
2016-04-02 14:13:05 -0700

    More prog. On layout guides

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,10 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Map loaded")
+    }
+    
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satelite"])
+        segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraintEqualToAnchor(view.topAnchor)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor)
+        topConstraint.active = true
+        leadingConstraint.active = true
+        trailingConstraint.active = true
     }
 }
